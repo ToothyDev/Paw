42be3d80afeb3dc2015c419cb63ee4ae99287bee
Joshua Slui <joshuaslui0203@gmail.com>
2023-04-18 16:25:08 +0200

    use @commands. decorator (bridge kills Greedy)

--- cogs/socials.py
+++ cogs/socials.py
@@ -1,3 +1,5 @@
+import discord
+
 import gifs
 from discord.ext import commands, bridge
 from utils import *
@@ -8,91 +10,91 @@ class socials(commands.Cog, name="social"):
         self.bot = bot
         self.help_icon = "♥️"
 
-    @bridge.bridge_command(brief="Snuggle someone")
+    @commands.command(brief="Snuggle someone")
     async def snuggle(self, ctx, members: commands.Greedy[discord.Member], *, reason=None):
         """Snuggle the specified people"""
         await interactions(ctx, members, "snuggled", 'snuggle', gifs.snuggle, reason)
 
-    @bridge.bridge_command(brief="Hug someone")
+    @commands.command(brief="Hug someone")
     async def hug(self, ctx, members: commands.Greedy[discord.Member], *, reason=None):
         await interactions(ctx, members, "hugged", 'hug', gifs.hug, reason, 'hug')
 
-    @bridge.bridge_command(brief="Boop someone")
+    @commands.command(brief="Boop someone")
     async def boop(self, ctx, members: commands.Greedy[discord.Member], *, reason=None):
         """Boop the specified people"""
         await interactions(ctx, members, "booped", 'boop', gifs.boop, reason)
 
-    @bridge.bridge_command(brief="Smooch someone", aliases=["kiss"])
+    @commands.command(brief="Smooch someone", aliases=["kiss"])
     async def smooch(self, ctx, members: commands.Greedy[discord.Member], *, reason=None):
         """Smooch the specified people"""
         await interactions(ctx, members, "smooched", 'smooch', gifs.smooch, reason)
 
-    @bridge.bridge_command(brief="Lick someone")
+    @commands.command(brief="Lick someone")
     async def lick(self, ctx, members: commands.Greedy[discord.Member], *, reason=None):
         """Lick the specified people"""
         await interactions(ctx, members, "licked", 'lick', gifs.lick, reason)
 
-    @bridge.bridge_command(brief="Give bellyrubs!")
+    @commands.command(brief="Give bellyrubs!")
     async def bellyrub(self, ctx, members: commands.Greedy[discord.Member], *, reason=None):
         """Give bellyrubs to the specified people"""
         await interactions(ctx, members, "bellyrubbed", 'rub the belly of', gifs.bellyrub, reason)
 
-    @bridge.bridge_command(brief="Nuzzle someone")
+    @commands.command(brief="Nuzzle someone")
     async def nuzzle(self, ctx, members: commands.Greedy[discord.Member], *, reason=None):
         """Nuzzle the specified people"""
         await interactions(ctx, members, "nuzzled", 'nuzzles', gifs.nuzzle, reason)
 
-    @bridge.bridge_command(brief="Cuddle someone")
+    @commands.command(brief="Cuddle someone")
     async def cuddle(self, ctx, members: commands.Greedy[discord.Member], *, reason=None):
         """Cuddle the specified people"""
         await interactions(ctx, members, "cuddled", 'cuddle', gifs.cuddle, reason)
 
-    @bridge.bridge_command(brief="Feed someone")
+    @commands.command(brief="Feed someone")
     async def feed(self, ctx, members: commands.Greedy[discord.Member], *, reason=None):
         """Feed the specified people"""
         await interactions(ctx, members, "fed", 'feed', gifs.feed, reason)
 
-    @bridge.bridge_command(brief="Glomp someone")
+    @commands.command(brief="Glomp someone")
     async def glomp(self, ctx, members: commands.Greedy[discord.Member], *, reason=None):
         """Glomp on the specified people"""
         await interactions(ctx, members, "glomped", 'glomp', gifs.glomp, reason)
 
-    @bridge.bridge_command(brief="Highfive someone")
+    @commands.command(brief="Highfive someone")
     async def highfive(self, ctx, members: commands.Greedy[discord.Member], *, reason=None):
         """Highfive the specified people"""
         await interactions(ctx, members, "highfived", 'hivefive', gifs.highfive, reason)
 
-    @bridge.bridge_command(brief="Rawrrrr")
+    @commands.command(brief="Rawrrrr")
     async def rawr(self, ctx, members: commands.Greedy[discord.Member], *, reason=None):
         """Rawr at the specified people"""
         await interactions(ctx, members, "rawred at", 'rawr at', gifs.rawr, reason)
 
-    @bridge.bridge_command(brief="Howl to the moon, or someone", aliases=["howl"])
+    @commands.command(brief="Howl to the moon, or someone", aliases=["howl"])
     async def awoo(self, ctx, members: commands.Greedy[discord.Member], *, reason=None):
         """Howl at the specified people"""
         await interactions(ctx, members, "howled at", 'howl at', gifs.awoo)
 
-    @bridge.bridge_command(brief="pat someone!", aliases=["pet"])
+    @commands.command(brief="pat someone!", aliases=["pet"])
     async def pat(self, ctx, members: commands.Greedy[discord.Member], *, reason=None):
         """Pat the specified people"""
         await interactions(ctx, members, "patted", 'pat', gifs.pet, reason, 'pat')
 
-    @bridge.bridge_command(brief="Gib cookie")
+    @commands.command(brief="Gib cookie")
     async def cookie(self, ctx, members: commands.Greedy[discord.Member], *, reason=None):
         """Give cookies to the specified people"""
         await interactions(ctx, members, "gave a cookie to", 'give a cookie to', gifs.cookie, reason)
 
-    @bridge.bridge_command(brief="Blushies!")
+    @commands.command(brief="Blushies!")
     async def blush(self, ctx, members: commands.Greedy[discord.Member]):
         """Blush (optionally because of specified people)"""
         await feelings(ctx, members, "blushes", gifs.blush)
 
-    @bridge.bridge_command(brief="Be happy")
+    @commands.command(brief="Be happy")
     async def happy(self, ctx, members: commands.Greedy[discord.Member]):
         """Be happy (optionally because of specified people)"""
         await feelings(ctx, members, "smiles", gifs.happy)
 
-    @bridge.bridge_command(brief="wag yer tail")
+    @commands.command(brief="wag yer tail")
     async def wag(self, ctx, members: commands.Greedy[discord.Member]):
         """Wag your tail (Optionally because of specified people)"""
         await feelings(ctx, members, "wags their tail", gifs.wag)
@@ -107,7 +109,7 @@ class socials(commands.Cog, name="social"):
             async with cs.get(facts) as r:
                 js = await r.json()
 
-                await ctx.send(js['fact'])
+                await ctx.respond(js['fact'])
 
     @bridge.bridge_command()
     @commands.cooldown(1, 5, commands.BucketType.user)
@@ -119,7 +121,7 @@ class socials(commands.Cog, name="social"):
 
                 e = discord.Embed(title="Floofy fox!", color=discord.Color.orange())
                 e.set_image(url=js['image'])
-                await ctx.send(embed=e)
+                await ctx.respond(embed=e)
 
     @bridge.bridge_command()
     @commands.cooldown(1, 5, commands.BucketType.user)
@@ -131,7 +133,7 @@ class socials(commands.Cog, name="social"):
         e = discord.Embed(color=discord.Color.random())
         e.set_image(url=link)
         e.set_footer(text=f"Gay avatar: {user}")
-        await ctx.send(embed=e)
+        await ctx.respond(embed=e)
 
 def setup(bot):
     bot.add_cog(socials(bot))
