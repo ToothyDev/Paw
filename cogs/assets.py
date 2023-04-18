b0b851a9e124ad156da271a0ac0006dfe5087907
Joshua Slui <joshuaslui0203@gmail.com>
2023-04-18 15:50:32 +0200

    what did i even do here

--- cogs/assets.py
+++ cogs/assets.py
@@ -4,13 +4,14 @@ import random
 import discord
 from utils import Colors
 
+
 class assets(commands.Cog, name="assets"):
     def __init__(self, bot):
         self.bot = bot
 
     @bridge.bridge_command(brief="Art of Paw")
     async def paw(self, ctx):
-        embed = discord.Embed(title="A picture of myself, Paw!",color=Colors.blue)
+        embed = discord.Embed(title="A picture of myself, Paw!", color=Colors.blue)
         embed.set_image(url=random.choice(gifs.paw))
         return await ctx.send(embed=embed)
 
