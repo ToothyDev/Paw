f60edcbed3e9dd7b7785adee5b5affdccfb61884
ToothyDev <chosenfate@gmx.de>
2023-04-18 18:01:27 +0200

    Add percentage to RAM usage

--- cogs/info.py
+++ cogs/info.py
@@ -17,8 +17,8 @@ class Info(commands.Cog):
 **Guilds:** {len(self.bot.guilds)}
 **Users:** {sum(x.member_count for x in self.bot.guilds)}
 **API Latency:** {round(self.bot.latency * 1000)}ms
-**RAM:** {psutil.virtual_memory().used / 1000000000} used out of {psutil.virtual_memory().total / 1000000000}GB total
-**Disk:** {psutil.disk_usage('/').free / 1000000000} free out of {psutil.disk_usage('/').total / 1000000000}GB total ({((psutil.disk_usage('/').percentage) - 100)*(-1)}% free)
+**RAM:** {psutil.virtual_memory().used / 1000000000} used out of {psutil.virtual_memory().total / 1000000000}GB total ({psutil.virtual_memory().percent}% used)
+**Disk:** {psutil.disk_usage('/').free / 1000000000} free out of {psutil.disk_usage('/').total / 1000000000}GB total ({((psutil.disk_usage('/').percent) - 100)*(-1)}% free)
 
 [[Github]](https://github.com/MiataBoy/iLoveMiatas) [[Privacy Policy]](https://gist.github.com/MiataBoy/20fda9024f277ea5eb2421adbebc2f23) [[Terms of Service]](https://gist.github.com/MiataBoy/81e96023a2aa055a038edab02e7e7792)
         """
