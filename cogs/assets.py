a23ae0436ede80a97c56507c643a7a297a099c54
ToothyDev <chosenfate@gmx.de>
2023-04-18 15:43:13 +0200

    Fix decorator, change color and fix arguments

--- cogs/assets.py
+++ cogs/assets.py
@@ -1,17 +1,18 @@
 import gifs
-from discord.ext import commands
+from discord.ext import commands, bridge
 import random
 import discord
+from utils import Colors
 
 class assets(commands.Cog, name="assets"):
     def __init__(self, bot):
         self.bot = bot
 
-@commands.command(brief="Art of Paw")
-async def paw(ctx):
-    embed = discord.Embed(title="A picture of myself, Paw!",color=discord.Color.blue())
-    embed.set_image(url=random.choice(gifs.paw))
-    return await ctx.send(embed=embed)
+    @bridge.bridge_command(brief="Art of Paw")
+    async def paw(self, ctx):
+        embed = discord.Embed(title="A picture of myself, Paw!",color=Colors.blue)
+        embed.set_image(url=random.choice(gifs.paw))
+        return await ctx.send(embed=embed)
 
-def setup(bot):
-    bot.add_cog(assets(bot))
+    def setup(bot):
+        bot.add_cog(assets(bot))
