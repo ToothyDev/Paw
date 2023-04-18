3406f8080c27c63c415f54101706fb7a365a38c4
ToothyDev <chosenfate@gmx.de>
2023-04-18 15:23:55 +0200

    Add rudimentary embed with a title and image

---
+++ cogs/assets.py
@@ -0,0 +1,17 @@
+import gifs
+from discord.ext import commands
+import random
+import discord
+
+class assets(commands.Cog, name="assets"):
+    def __init__(self, bot):
+        self.bot = bot
+
+@commands.command(brief="Art of Paw")
+async def paw(ctx):
+    embed = discord.Embed(title="A picture of myself, Paw!",color=discord.Color.blue())
+    embed.set_image(url=random.choice(gifs.paw))
+    return await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(assets(bot))
