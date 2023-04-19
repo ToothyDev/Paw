9ef730ed4c7543b1de6b0a2683e16cc6398edd72
ToothyDev <chosenfate@gmx.de>
2023-04-19 21:27:52 +0200

    Potentially fix hex color, and make heights better

--- cogs/utility.py
+++ cogs/utility.py
@@ -12,16 +12,19 @@ class utility(commands.Cog, name="utility"):
         primary_color = "%06x" % random.randint(0, 0xFFFFFF);
         colors = ["Red", "Green", "Blue", "Pink", "Purple", "Brown", "Black", "White", "Orange", "Teal", "Light Green", "Light Blue", "Grey", "Yellow"]
         species = ["Dragon", "Fox", "Deer", "Wolf", "Dog", "Bunny", "Protogen", "Hyena", "Tiger", "Lion", "Bird", "Otter", "Snake", "Cat", "Sergal", "Horse", "Shark", "Lizard"]
-        sonatype = ["Feral", "Anthro"]
+        sonatype = random.choice(["Feral", "Anthro"])
         sex = ["Male", "Male", "Male", "Male", "Female", "Female", "Female", "Female", "Hermaphrodite"]
 
-        embed=discord.Embed(title="Your Sona:", color=primary_color)
+        embed=discord.Embed(title="Your Sona:", color=int(primary_color))
         embed.add_field(name="Primary Color:", value=f"{primary_color} (see embed color)", inline=True)
         embed.add_field(name="Secondary Color:", value=random.choice(colors), inline=True)
         embed.add_field(name="Species:", value=f"{random.choice(sonatype)} {random.choice(species)}", inline=True)
         embed.add_field(name="Sex:", value=random.choice(sex), inline=True)
-        embed.add_field(name="Size:", value=f"{random(120,240)}cm", inline=True)
-        
+        if sonatype == "Feral":
+            embed.add_field(name="Length:", value=f"{random(80,560)}cm", inline=True)
+        else:
+            embed.add_field(name="Height:", value=f"{random(120,240)}cm", inline=True)
+
         return await ctx.respond("Sure, here's your freshly generated sona!", embed=embed)
 
 
