1fe320f5ee89389560876b669116ebb6680808ec
ToothyDev <chosenfate@gmx.de>
2023-04-19 23:08:40 +0200

    Reformat embed content to use its description

--- cogs/utility.py
+++ cogs/utility.py
@@ -15,18 +15,18 @@ class utility(commands.Cog, name="utility"):
         species = random.choice(data.species)
         sonatype = random.choice(["Feral", "Anthro"])
         sex = random.choice(["Male", "Male", "Male", "Male", "Female", "Female", "Female", "Female", "Hermaphrodite"])
-
-        embed=discord.Embed(title="Your Sona:", color=primary_color)
-        embed.add_field(name="Species:", value=f"{sonatype} {species}", inline=True)
-        embed.add_field(name="Primary Color:", value=f"#{'{:06x}'.format(primary_color)} (embed color)", inline=True)
-        embed.add_field(name="Secondary Color:", value=color, inline=True)
         if sonatype == "Feral":
-            embed.add_field(name="Height to shoulders:", value=f"{random.randint(20,140)}cm", inline=True)
+            heightstring = f"**Height to shoulders**: {random.randint(20,140)}cm"
         else:
-            embed.add_field(name="Height:", value=f"{random.randint(120,240)}cm", inline=True)
+            heightstring = f"**Height**: {random.randint(120,240)}cm"
 
-        embed.add_field(name="Sex:", value=sex, inline=True)
-        
+        embed=discord.Embed(title="Your Sona:", color=primary_color, description=f"""
+            **Species**: {sonatype} {species}
+            **Primary Color**: #{'{:06x}'.format(primary_color)} (embed color)
+            **Secondary Color**: {color}
+            {heightstring}
+            **Sex**: {sex}
+            """)
 
         return await ctx.respond("Sure, here's your freshly generated sona!", embed=embed)
     
