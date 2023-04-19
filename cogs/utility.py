8af7c37b4abf5790e949716d230bda847c16e12f
ToothyDev <chosenfate@gmx.de>
2023-04-19 22:55:19 +0200

    Enforce E251

--- cogs/utility.py
+++ cogs/utility.py
@@ -16,7 +16,7 @@ class utility(commands.Cog, name="utility"):
         sonatype = random.choice(["Feral", "Anthro"])
         sex = random.choice(["Male", "Male", "Male", "Male", "Female", "Female", "Female", "Female", "Hermaphrodite"])
 
-        embed=discord.Embed(title="Your Sona:", color = primary_color)
+        embed=discord.Embed(title="Your Sona:", color=primary_color)
         embed.add_field(name="Species:", value=f"{sonatype} {species}", inline=True)
         embed.add_field(name="Primary Color:", value=f"#{'{:06x}'.format(primary_color)} (embed color)", inline=True)
         embed.add_field(name="Secondary Color:", value=color, inline=True)
@@ -24,7 +24,7 @@ class utility(commands.Cog, name="utility"):
             embed.add_field(name="Height to shoulders:", value=f"{random.randint(20,140)}cm", inline=True)
         else:
             embed.add_field(name="Height:", value=f"{random.randint(120,240)}cm", inline=True)
-        
+
         embed.add_field(name="Sex:", value=sex, inline=True)
         
 
