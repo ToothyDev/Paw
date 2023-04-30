b6729d937e8d44569539ca0a57d86b8cd1fcc80f
ToothyDev <chosenfate@gmx.de>
2023-04-30 19:06:44 +0200

    Make error handler work for everything

--- cogs/events/errors.py
+++ cogs/events/errors.py
@@ -10,29 +10,51 @@ class error(commands.Cog, name="Error"):
         self.bot = bot
 
     @commands.Cog.listener()
+    async def on_application_command_error(self, ctx, err):
+        if isinstance(err, commands.CommandNotFound):
+            return
+
+        if isinstance(err, commands.MissingPermissions):
+            perms = "`" + '`, `'.join(err.missing_permissions) + "`"
+            return await ctx.respond(f"{config.crossmark} **You are missing {perms} permissions.**", ephemeral=True)
+
+        if isinstance(err, commands.BotMissingPermissions):
+            perms = "`" + '`, `'.join(err.missing_permissions) + "`"
+            return await ctx.respond(f"{config.crossmark} **I'm missing {perms} permissions**", ephemeral=True)
+
+        if isinstance(err, commands.CommandOnCooldown):
+            return await ctx.respond(f"{config.crossmark} **This command is on cooldown for {round(err.retry_after)} more seconds.**", ephemeral=True)
+
+        if isinstance(err, commands.MemberNotFound):
+            return await ctx.respond(f"{config.confused} **Could not find user `{err.argument}`", ephemeral=True)
+
+        if isinstance(err, discord.NotFound):
+            return await ctx.respond("I could not find the argument you have provided.", ephemeral=True)
+
+    @commands.Cog.listener()
     async def on_command_error(self, ctx, err):
         if isinstance(err, commands.CommandNotFound):
             return
 
         if isinstance(err, commands.MissingPermissions):
             perms = "`" + '`, `'.join(err.missing_permissions) + "`"
-            return await ctx.send("{} **You are missing {} permissions.**".format(config.crossmark, perms))
+            return await ctx.respond(f"{config.crossmark} **You are missing {perms} permissions.**")
 
         if isinstance(err, commands.BotMissingPermissions):
             perms = "`" + '`, `'.join(err.missing_permissions) + "`"
-            return await ctx.send("{} **I'm missing {} permissions**".format(config.crossmark, perms))
+            return await ctx.respond(f"{config.crossmark} **I'm missing {perms} permissions**")
 
         if isinstance(err, commands.MissingRequiredArgument):
-            return await ctx.send("{} **`{}` is a required argument!**".format(config.crossmark, err.param.name))
+            return await ctx.send(f"{config.crossmark} **`{err.param.name}` is a required argument!**")
 
         if isinstance(err, commands.CommandOnCooldown):
-            return await ctx.send("{} **This command is on cooldown for __{:.0f}__ more seconds.**".format(config.crossmark,err.retry_after))
+            return await ctx.respond(f"{config.crossmark} **This command is on cooldown for {round(err.retry_after)} more seconds.**")
 
         if isinstance(err, commands.MemberNotFound):
-            return await ctx.send("{0} **Could not find user `{1}`**".format(config.confused, err.argument))
+            return await ctx.respond(f"{config.confused} **Could not find user `{err.argument}`")
 
         if isinstance(err, discord.NotFound):
-            return await ctx.send("I could not find the argument you have provided.")
+            return await ctx.respond("I could not find the argument you have provided.")
 
 
 def setup(bot):
