fb3e3cf277f977b929fa803cda7d9fd03f0f5cdb
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-07 21:44:29 +0200

    Limit command to five people

--- utils.py
+++ utils.py
@@ -66,7 +66,9 @@ async def mentionconverter(self, ctx, members):
         member = await guild.fetch_member(member)
         memberlist.append(member)
     if not memberlist:
-        return await ctx.respond('Sorry, but you need to specify someone with a mention.')
+        return await ctx.respond('Sorry, but you need to specify someone with a mention.', ephemeral=True)
+    if len(memberlist) > 5:
+        return await ctx.respond('Sorry, but this command is limited to 5 people.', ephemeral=True)
     return memberlist
 
 
