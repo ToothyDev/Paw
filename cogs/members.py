9203fe9fb936a27ef370fa88223f0d1467b1e5d5
ToothyDev <chosenfate@gmx.de>
2023-05-09 18:49:40 +0200

    Move members.py into cogs for commands

--- cogs/members.py
+++ cogs/members.py
@@ -1,6 +1,8 @@
 from discord.ext import commands
 import utils
 import time
+import discord
+from discord import slash_command
 
 
 class Members(commands.Cog, name="Members"):
@@ -12,6 +14,11 @@ class Members(commands.Cog, name="Members"):
     async def on_member_join(self, member):
         if member.guild.id == 715969701771083817:
             self.memberkicker.addMember((member.id, time.time()))
+
+    @slash_command()
+    @discord.default_permissions(manage_guild=True)
+    async def getinactive(self, ctx):
+        await ctx.respond(self.memberkicker.getMembers())
 
 
 def setup(bot):
