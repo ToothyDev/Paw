74a67af0c615efb2fec23f1d9c64dd1c7c983ce8
ToothyDev <chosenfate@gmx.de>
2023-05-09 18:00:20 +0200

    Add simple member join listener

--- cogs/events/members.py
+++ cogs/events/members.py
@@ -1,13 +1,17 @@
-import discord
-import config
 from discord.ext import commands
+import utils
+import time
 
-
-# from cogs.admin import admin_only
 
 class Members(commands.Cog, name="Members"):
     def __init__(self, bot):
         self.bot = bot
+        self.memberkicker = utils.AutoVerify(self.bot)
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        print(f"Member joined! {member.name}")
+        self.memberkicker.addMember((member.id, member.guild.id, time.time()))
 
 
 def setup(bot):
