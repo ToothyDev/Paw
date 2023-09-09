20acea95f56bfc5a9b020a92e759c5246ff4c07d
ToothyDev <chosenfate@gmx.de>
2023-09-10 01:29:47 +0200

    Third attempt

--- utils.py
+++ utils.py
@@ -162,7 +162,7 @@ class AutoVerify():
             if not time.time() > (timestamp + 259200):  # check if 3 days have passed, if not, continue with next member
                 continue
             if not any(role.id in self.roles for role in member.roles):
-                if (time.time() < (timestamp + 2592000)) and (not added):
+                if (time.time() < (timestamp + 2592000)) and (added is False):
                     output += f"<@{member.id}> **|** "
                     added = True
                 else:
