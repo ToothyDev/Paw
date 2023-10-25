5784cb64caa767f59400abeacb00c234b8021d14
ToothyDev <chosenfate@gmx.de>
2023-10-25 16:23:01 +0200

    Change inactive time to 7 days

--- utils.py
+++ utils.py
@@ -194,7 +194,7 @@ class AutoVerify():
             if (time.time() - timestamp) < 259200:  # check if 3 days have passed, if not, continue with next member
                 continue
             if not any(role.id in self.roles for role in member.roles):
-                if (time.time() - timestamp) < 2592000:
+                if (time.time() - timestamp) < 604800:
                     if added is False:
                         output += f"**|** <@{member.id}> "
                         added = True
