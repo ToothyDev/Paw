7edbf50e8ad0585eb90ffb0c0f607d273135a290
ToothyDev <chosenfate@gmx.de>
2023-11-14 12:53:34 +0100

    Add a log message upon bot kick

--- cogs/members.py
+++ cogs/members.py
@@ -16,7 +16,7 @@ class Members(commands.Cog, name="Members"):
             await self.inactives_checker.addMember((member.id, time.time()))
 
     @commands.Cog.listener()
-    async def on_member_update(self, member, member_new):
+    async def on_member_update(self, member, member_new: discord.Member):
         if member_new.guild.id == 715969701771083817:
             botroles_list = [891021633505071174, 731233454716354710]  # Red, Bear
             botroles_list2 = [891021633505071174, 731233454716354710, 731245341495787541,
@@ -36,6 +36,17 @@ class Members(commands.Cog, name="Members"):
                     print(f"Unable to kick bot {member.display_name} ({member.id}). Error:\n{e}")
                     return
                 channel = member_new.guild.get_channel(1066357407443333190)
+                logchannel = member_new.guild.get_channel(760181839033139260)
+                embed = discord.Embed(color=utils.Colors.orange)
+                embed.set_author(name=f"Bot Kick | {member_new.display_name}", icon_url=member_new.avatar.url)
+                embed.set_footer(text=member_new.id)
+                embed.description = f"**User**: {member_new.mention}\n**User ID**: {member_new.id}"
+                await logchannel.send(embed=embed)
+                embed = discord.Embed(color=utils.Colors.orange)
+                embed.set_author(name=f"Bot Kick | {member_new.display_name}", icon_url=member_new.avatar.url)
+                embed.set_footer(text=member_new.id)
+                embed.description = f"**User**: {member_new.mention}\n**User ID**: {member_new.id}"
+                await logchannel.send(embed=embed)
                 async for message in channel.history(limit=15):
                     if member in message.content.mentions:
                         await message.delete(reason="Deleting bot join message")
