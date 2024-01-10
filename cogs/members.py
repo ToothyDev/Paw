ddeae15a8163da9796366fcb821d8059db1eb8b9
ToothyDev <chosenfate@gmx.de>
2024-01-10 22:09:23 +0100

    Replace dyno's embed with a custommade version to handle bots

--- cogs/members.py
+++ cogs/members.py
@@ -1,4 +1,5 @@
 import time
+import asyncio
 from discord.ext import commands
 import discord
 from discord import SlashCommandGroup
@@ -14,6 +15,16 @@ class Members(commands.Cog, name="Members"):
     async def on_member_join(self, member):
         if member.guild.id == 715969701771083817:
             await self.inactives_checker.addMember((member.id, time.time()))
+            await asyncio.sleep(20)
+            if member in member.guild.members:
+                channel = member.guild.get_channel(1066357407443333190)
+                embed = discord.Embed(color=utils.Colors.purple)
+                embed.set_thumbnail(url=member.display_avatar)
+                embed.description = f"""
+Welcome to the server, {member.mention}!\nFeel free to visit <id:customize> for roles & channels and <id:guide> for some useful info!
+__**IMPORTANT**__: To gain access to the rest of the server,you need to first gain a level by chatting in this channel.
+Thank you for reading and have fun!"""
+                await channel.send(content=f"<@&822886791312703518>, welcome {member.mention}", embed=embed)
 
     @commands.Cog.listener()
     async def on_member_update(self, member, member_new: discord.Member):
