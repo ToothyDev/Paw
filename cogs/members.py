3d7d8102d0e2dbb794a9f5385b2e23ecf0866c02
ToothyDev <chosenfate@gmx.de>
2024-04-13 22:27:24 +0200

    Don't do any checks for genuine bots

--- cogs/members.py
+++ cogs/members.py
@@ -14,19 +14,22 @@ class Members(discord.Cog, name="Members"):
     @discord.Cog.listener()
     async def on_member_join(self, member):
         if member.guild.id == 715969701771083817:
-            await asyncio.sleep(20)
-            if member in member.guild.members:  # If member isn't a bot (95% accurate)
-                if await botchecker(member):
-                    return
-                channel = member.guild.get_channel(1066357407443333190)
-                embed = discord.Embed(color=Colors.purple)
-                embed.set_thumbnail(url=member.display_avatar)
-                embed.description = f"""
+            if member.bot:  # If member is an ACTUAL bot
+                return
+            await asyncio.sleep(20)  # Timer to let bots assign roles
+            if member not in member.guild.members:
+                return
+            if await botchecker(member):  # If member is a bot (95% accurate)
+                return
+            channel = member.guild.get_channel(1066357407443333190)
+            embed = discord.Embed(color=Colors.purple)
+            embed.set_thumbnail(url=member.display_avatar)
+            embed.description = f"""
 Welcome to the server, {member.mention}!\nFeel free to visit <id:customize> for roles & channels and <id:guide> for some useful info!
 __**IMPORTANT**__: To gain access to the rest of the server, you need to first gain a level by chatting in this channel.
 Thank you for reading and have fun!"""
-                await channel.send(content=f"<@&822886791312703518>, welcome {member.mention}", embed=embed)
-                await self.inactives_checker.add_member((member.id, time.time()))
+            await channel.send(content=f"<@&822886791312703518>, welcome {member.mention}", embed=embed)
+            await self.inactives_checker.add_member((member.id, time.time()))
 
     @discord.Cog.listener()
     async def on_member_update(self, member, member_new: discord.Member):
