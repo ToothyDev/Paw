703096572353f229b1b080b0ff9c4623c3a4fc00
ToothyDev <chosenfate@gmx.de>
2024-05-04 22:35:58 +0200

    Initial working version of Groq

--- ai_handler.py
+++ ai_handler.py
@@ -1,26 +1,33 @@
 import discord
 import google.generativeai as genai
+from groq import Groq
 
 import config
 
 
-async def generate(prompt: str):
-    genai.configure(api_key=config.api_key)
-    model = genai.GenerativeModel("gemini-pro")
-    response = await model.generate_content_async(prompt)
-    return response.text
+# async def generate(prompt: str):
+#    genai.configure(api_key=config.api_key)
+#    model = genai.GenerativeModel("gemini-pro")
+#    response = await model.generate_content_async(prompt)
+#    return response.text
 
 
-async def generate_stream(ctx: discord.ApplicationContext, prompt: str):
-    genai.configure(api_key=config.api_key)
-    model = genai.GenerativeModel("gemini-pro")
-    response = await model.generate_content_async(prompt, stream=True)
-    edited = False
-    text = ""
-    async for chunk in response:
-        text += chunk.text
-        if edited:
-            await ctx.edit(content=text)
-        else:
-            await ctx.respond(text)
-            edited = True
+# async def generate_stream(ctx: discord.ApplicationContext, prompt: str):
+#    genai.configure(api_key=config.api_key)
+#    model = genai.GenerativeModel("gemini-pro")
+#    response = await model.generate_content_async(prompt, stream=True)
+#    edited = False
+#    text = ""
+#    async for chunk in response:
+#        text += chunk.text
+#        if edited:
+#            await ctx.edit(content=text)
+#        else:
+#            await ctx.respond(text)
+#            edited = True
+
+async def generate(prompt: str) -> str:
+    client = Groq(api_key=config.groq_api_key)
+    chat_completion = client.chat.completions.create(messages=[{"role": "user", "content": prompt}],
+                                                     model="llama3-70b-8192")
+    return chat_completion.choices[0].message.content
