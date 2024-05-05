8919b448cb353a1392ea8f9d2aa03119618c181c
ToothyDev <chosenfate@gmx.de>
2024-05-05 22:50:13 +0200

    Apply requested changes

--- ai_handler.py
+++ ai_handler.py
@@ -1,31 +1,6 @@
-# import discord
-# import google.generativeai as genai
 from groq import AsyncGroq
-
 import config
 
-
-# Old google snippets, might become useful later
-# async def generate(prompt: str):
-#    genai.configure(api_key=config.api_key)
-#    model = genai.GenerativeModel("gemini-pro")
-#    response = await model.generate_content_async(prompt)
-#    return response.text
-
-
-# async def generate_stream(ctx: discord.ApplicationContext, prompt: str):
-#    genai.configure(api_key=config.api_key)
-#    model = genai.GenerativeModel("gemini-pro")
-#    response = await model.generate_content_async(prompt, stream=True)
-#    edited = False
-#    text = ""
-#    async for chunk in response:
-#        text += chunk.text
-#        if edited:
-#            await ctx.edit(content=text)
-#        else:
-#            await ctx.respond(text)
-#            edited = True
 
 async def generate_from_history(history: list[dict]) -> str:
     client = AsyncGroq(api_key=config.groq_api_key)
