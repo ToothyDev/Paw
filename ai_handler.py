d63f3878211143d1614adebbf079ee50f6c028af
ToothyDev <chosenfate@gmx.de>
2024-05-10 18:31:28 +0200

    Make both generator functions use max char limit

--- ai_handler.py
+++ ai_handler.py
@@ -11,5 +11,5 @@ async def generate_from_history(history: list[dict]) -> str:
 async def generate_single(prompt: str) -> str:
     client = AsyncGroq(api_key=config.groq_api_key)
     chat_completion = await client.chat.completions.create(messages=[{"role": "user", "content": prompt}],
-                                                           model="llama3-70b-8192")
+                                                           model="llama3-70b-8192", max_tokens=400)
     return chat_completion.choices[0].message.content
