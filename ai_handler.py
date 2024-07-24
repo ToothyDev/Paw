0f9d273c3a8402543976675221646ac4c20a3943
ToothyDev <chosenfate@gmx.de>
2024-07-24 23:27:37 +0200

    Upgrade to new llama3.1 model of same parameter size

--- ai_handler.py
+++ ai_handler.py
@@ -5,20 +5,21 @@ from utils import Fursona
 
 async def generate_from_history(history: list[dict]) -> str:
     client = AsyncGroq(api_key=config.groq_api_key)
-    chat_completion = await client.chat.completions.create(messages=history, model="llama3-70b-8192", max_tokens=400)
+    chat_completion = await client.chat.completions.create(messages=history, model="llama-3.1-70b-versatile",
+                                                           max_tokens=400)
     return chat_completion.choices[0].message.content
 
 
 async def generate_single(prompt: str) -> str:
     client = AsyncGroq(api_key=config.groq_api_key)
     chat_completion = await client.chat.completions.create(messages=[{"role": "user", "content": prompt}],
-                                                           model="llama3-70b-8192", max_tokens=400)
+                                                           model="llama-3.1-70b-versatile", max_tokens=400)
     return chat_completion.choices[0].message.content
 
 
 async def generate_sona(prompt: str) -> Fursona:
     client = AsyncGroq(api_key=config.groq_api_key)
     chat_completion = await client.chat.completions.create(messages=[{"role": "user", "content": prompt}],
-                                                           model="llama3-70b-8192",
+                                                           model="llama-3.1-70b-versatile",
                                                            response_format={"type": "json_object"})
     return Fursona.model_validate_json(chat_completion.choices[0].message.content)
