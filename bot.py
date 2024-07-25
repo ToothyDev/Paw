16533ce6633f4dd31aa2d09b7fbdebaaa56f346f
ToothyDev <chosenfate@gmx.de>
2024-07-25 20:34:47 +0200

    Clean up main bot file

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import discord
 from discord import Intents, Status, Activity, ActivityType
-from discord.ext import commands
 
 from config import token
 
@@ -8,21 +7,9 @@ intents = Intents(guilds=True, guild_messages=True, message_content=True, member
 
 bot = discord.Bot(intents=intents, status=Status.dnd, activity=Activity(type=ActivityType.watching, name="you"))
 
-
-class MyNewHelp(commands.MinimalHelpCommand):
-    async def send_pages(self):
-        destination = self.get_destination()
-        for page in self.paginator.pages:
-            emby = discord.Embed(description=page)
-            await destination.send(embed=emby)
-
-
-bot.help_command = MyNewHelp()
-
-bot.load_extensions("cogs")  # Loads all cogs in the cogs folder
+bot.load_extensions("cogs")
 bot.load_extensions("cogs.events")
 print(bot.extensions)
-BOOTED = False
 
 
 @bot.listen()
@@ -32,14 +19,8 @@ async def on_connect():
 
 @bot.listen()
 async def on_ready():
-    global BOOTED
-    if BOOTED:
-        print("Reconnect(?)")
-    if not BOOTED:
-        # await bot.sync_commands() #You might need to uncomment this if the slash commands aren't appearing
-        print(f'Logged in as {bot.user}')
-        print('------')
-        BOOTED = True
+    print(f'Logged in as {bot.user}')
+    print('------')
 
 
 bot.run(token)
