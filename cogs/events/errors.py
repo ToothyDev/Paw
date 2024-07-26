7cb890b90a474a081fea27d5f1cc3224cf451112
ToothyDev <chosenfate@gmx.de>
2024-07-26 22:54:37 +0200

    Improve error handling

--- cogs/events/errors.py
+++ cogs/events/errors.py
@@ -11,7 +11,7 @@ class Error(discord.Cog, name="Errors"):
 
     @commands.Cog.listener()
     async def on_application_command_error(self, ctx: discord.ApplicationContext,
-                                           err: discord.DiscordException | groq.APIError):
+                                           err: discord.ApplicationCommandInvokeError):
         if isinstance(err, commands.MissingPermissions):
             perms = "`" + '`, `'.join(err.missing_permissions) + "`"
             return await ctx.respond(f"{config.crossmark} **You are missing {perms} permissions.**", ephemeral=True)
@@ -21,7 +21,9 @@ class Error(discord.Cog, name="Errors"):
             return await ctx.respond(f"{config.crossmark} **I'm missing {perms} permissions**", ephemeral=True)
 
         if isinstance(err, commands.CommandOnCooldown):
-            return await ctx.respond(f"{config.crossmark} **This command is on cooldown for {round(err.retry_after)} more seconds.**", ephemeral=True)
+            return await ctx.respond(
+                f"{config.crossmark} **This command is on co!!!oldown for {round(err.retry_after)} more seconds.**",
+                ephemeral=True)
 
         if isinstance(err, commands.MemberNotFound):
             return await ctx.respond(f"{config.confused} **Could not find user `{err.argument}`", ephemeral=True)
@@ -29,11 +31,14 @@ class Error(discord.Cog, name="Errors"):
         if isinstance(err, discord.NotFound):
             return await ctx.respond("I could not find the argument you have provided.", ephemeral=True)
 
+        err = err.original  # Unwrap the exception to catch any non-discord errors
+
         if isinstance(err, groq.RateLimitError):
-            return await ctx.respond(f"You are using this command too much! {err.message.split('.')[1]}s")
+            return await ctx.respond(f"You are using this command too much! {err.message.split('.')[1]}s",
+                                     ephemeral=True)
 
         await ctx.respond("An unknown error occured! This will be logged and fixed!", ephemeral=True)
-        print(err)
+        print(f"{ctx.author.global_name} used /{ctx.command} which caused {err}")
 
 
 def setup(bot):
