8cbc8392d9ba6f23c167a6ec41f27bd31d6f5b2b
ToothyDev <chosenfate@gmx.de>
2024-08-22 16:09:40 +0200

    Add initial version of spammer kicker

--- cogs/events/members.py
+++ cogs/events/members.py
@@ -1,11 +1,33 @@
 import discord
 
 import utils
+from utils import Colors
 
 
 class MemberEvents(discord.Cog, name="Member Events"):
     def __init__(self, bot: discord.Bot):
         self.bot = bot
+
+    @discord.Cog.listener()
+    async def on_member_join(self, member: discord.Member):
+        if member.public_flags.value & 1048576 == 1048576:
+            try:
+                await member.send(
+                    "You've been kicked from The Paw Kingdom for being flagged as spammer.")
+            except discord.Forbidden:
+                pass
+            except discord.HTTPException as e:
+                return print(f"Kicking member {member.display_name} failed {e}")
+            try:
+                await member.kick(reason="Spammer")
+            except Exception as e:
+                return print(f"Unable to kick bot {member.display_name} ({member.id}). Error:\n{e}")
+            embed = discord.Embed(color=Colors.orange)
+            embed.set_author(name=f"Spammer Kick | {member.display_name}", icon_url=member.display_avatar.url)
+            embed.set_footer(text=member.id)
+            embed.description = f"**User**: {member.mention}\n**User ID**: {member.id}"
+            logchannel = member.guild.get_channel(760181839033139260)
+            await logchannel.send(embed=embed)
 
     @discord.Cog.listener()
     async def on_member_update(self, member_old: discord.Member, member: discord.Member):
