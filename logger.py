02b31543b41a662a848c994f0f60247a6dcad7b2
ToothyDev <chosenfate@gmx.de>
2024-08-30 12:42:13 +0200

    Remove unnecessary newlines

--- logger.py
+++ logger.py
@@ -9,15 +9,12 @@ level_styles = {
     'critical': {'color': 'red', 'bold': True},
 }
 
-
 def configure_logging():
     coloredlogs.install(level='INFO', level_styles=level_styles,
                         fmt='%(asctime)s %(name)s %(levelname)s %(message)s')
 
-
 def get_logger(name):
     return logging.getLogger(name)
-
 
 # Call configure_logging to set up the colored logs
 configure_logging()
