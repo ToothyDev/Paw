77018c922070868d19690a61d4c5aa010d5ac66c
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2024-12-25 17:40:31 -0500

    Update example.config.py

--- example.config.py
+++ example.config.py
@@ -1,5 +1,5 @@
 token = "BOT_TOKEN"
-ai_api_key = "GROQ_API_KEY"
+ai_api_key = "GROQ_OR_OPENAI_API_KEY" # You can insert a OpenAI or Groq API key here, no other changes are neccessary to make either work, the code will automatically detect which one you're using.
 text_model = "llama-3.3-70b-specdec"
 vision_model = "llama-3.2-11b-vision-preview"
 
