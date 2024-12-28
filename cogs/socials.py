3834a0804b3cb502aca2e7c16e2c05eec2849b70
ToothyDev <chosenfate@gmx.de>
2024-12-29 00:49:17 +0100

    Reorder and privatize functions

--- cogs/socials.py
+++ cogs/socials.py
@@ -15,12 +15,12 @@ class Socials(discord.Cog, name="Socials"):
         self.last_revived = 0
 
         for command in utils.data.SOCIAL_COMMANDS_DATA:
-            self.create_social_command(command["name"], command["description"], command["option_description"],
-                                       command["words"], command["gifs"])
+            self._create_social_command(command["name"], command["description"], command["option_description"],
+                                        command["words"], command["gifs"])
 
         for command in utils.data.EMOTION_COMMANDS_DATA:
-            self.create_emotion_command(command["name"], command["description"], command["option_description"],
-                                        command["word"], command["gifs"])
+            self._create_emotion_command(command["name"], command["description"], command["option_description"],
+                                         command["word"], command["gifs"])
 
     @slash_command()
     @option("topic", str, description="The topic to revive chat with", required=False)
@@ -36,42 +36,6 @@ class Socials(discord.Cog, name="Socials"):
         await ctx.respond(
             f"<@&738356235841175594>! {topic if topic else 'Talk about something interesting!'}",
             allowed_mentions=discord.AllowedMentions(everyone=False, roles=[revival_role]))
-
-    def create_social_command(self, name: str, description: str, option_description: str, words: list[str],
-                              gifs: list[str]):
-
-        async def command_function(ctx: discord.ApplicationContext, members: str):
-            await utils.social_interaction_handler(ctx, members, words, gifs)
-
-        command = discord.SlashCommand(command_function,
-                                       name=name,
-                                       description=description,
-                                       options=[
-                                           discord.Option(name="members",
-                                                          description=option_description)
-                                       ])
-        self.bot.add_application_command(command)
-
-    def create_emotion_command(self, name: str, description: str, option_description: str, word: str, gifs: list[str]):
-
-        async def command_function(ctx: discord.ApplicationContext, members: str):
-            if not members:
-                memberlist = None
-            else:
-                memberlist = await utils.mention_converter(ctx, members)
-                if not memberlist:
-                    return
-            await utils.feelings(ctx, memberlist, word, gifs)
-
-        command = discord.SlashCommand(command_function,
-                                       name=name,
-                                       description=description,
-                                       options=[
-                                           discord.Option(name="members",
-                                                          description=option_description,
-                                                          required=False)
-                                       ])
-        self.bot.add_application_command(command)
 
     @slash_command()
     @commands.cooldown(1, 5, commands.BucketType.user)
@@ -113,6 +77,42 @@ class Socials(discord.Cog, name="Socials"):
         input_history = await build_input_history(self.bot, ctx, text)
         response = await utils.generate_from_history(input_history)
         await ctx.respond(content=f"**Prompt:** {text}\n**Paw:** {response}")
+
+    def _create_social_command(self, name: str, description: str, option_description: str, words: list[str],
+                               gifs: list[str]):
+
+        async def command_function(ctx: discord.ApplicationContext, members: str):
+            await utils.social_interaction_handler(ctx, members, words, gifs)
+
+        command = discord.SlashCommand(command_function,
+                                       name=name,
+                                       description=description,
+                                       options=[
+                                           discord.Option(name="members",
+                                                          description=option_description)
+                                       ])
+        self.bot.add_application_command(command)
+
+    def _create_emotion_command(self, name: str, description: str, option_description: str, word: str, gifs: list[str]):
+
+        async def command_function(ctx: discord.ApplicationContext, members: str):
+            if not members:
+                memberlist = None
+            else:
+                memberlist = await utils.mention_converter(ctx, members)
+                if not memberlist:
+                    return
+            await utils.feelings(ctx, memberlist, word, gifs)
+
+        command = discord.SlashCommand(command_function,
+                                       name=name,
+                                       description=description,
+                                       options=[
+                                           discord.Option(name="members",
+                                                          description=option_description,
+                                                          required=False)
+                                       ])
+        self.bot.add_application_command(command)
 
 
 def setup(bot):
